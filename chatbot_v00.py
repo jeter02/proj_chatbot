b6afc9f23f6dc6ca070b4f943c2bfc4bcf6faa18
jeter02 <sj.0713@gmail.com>
2019-10-29 22:50:40 +0900

    to test bot-sample

--- chatbot_v00.py
+++ chatbot_v00.py
@@ -1,65 +1,36 @@
-﻿from telegram.ext import Updater, MessageHandler, Filters
-from emoji import emojize
-import requests
-from bs4 import BeautifulSoup
+﻿import os
 
-updater = Updater(token='719614337:AAGVXDHZtOK7YNUsokdtbgjqExNoTEMXxzA')
-dispatcher = updater.dispatcher
-updater.start_polling()
+from flask import Flask, request
 
-url_1 = 'https://www.naver.com'
-url_2 = 'https://www.clien.net/service/board/news'
-url_3 = 'https://www.clien.net/service/board/cm_car'
+import telebot
+
+TOKEN = '719614337:AAGVXDHZtOK7YNUsokdtbgjqExNoTEMXxzA'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 
-class WebCrwl:
-    def __init__(self, URL):
-        self.url = URL
-
-    def naver_rt(self):
-        resp = requests.get(self.url)
-        soup = BeautifulSoup(resp.text, 'html.parser')
-        titles = soup.select('.ah_roll .ah_k')
-        return titles
-
-    def clien_bbs(self):
-        resp = requests.get(self.url)
-        soup = BeautifulSoup(resp.text, 'html.parser')
-        titles = soup.select('.list_subject .subject_fixed')
-        return titles
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-class Bitcoin:
-    pass
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
-## telegram handler ###
-
-def handler(bot, update):
-    text = update.message.text
-    chat_id = update.message.chat_id
-
-    if '실시간' in text:
-        a = WebCrwl(url_1)
-        titles = a.naver_rt()
-        for title in titles:
-            bot.send_message(chat_id=chat_id, text=title.get_text())
-    elif '새소식' in text:
-        a = WebCrwl(url_2)
-        titles = a.clien_bbs()
-        for title in titles:
-            bot.send_message(chat_id=chat_id, text=title.get_text())
-    elif '굴당' or '굴러간당' in text:
-        a = WebCrwl(url_3)
-        titles = a.clien_bbs()
-        for title in titles:
-            bot.send_message(chat_id=chat_id, text=title.get_text())
-
-    else:
-        bot.send_message(chat_id=chat_id, text='다시 입력해 주세요')
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
-print('now running...')
-echo_handler = MessageHandler(Filters.text, handler)
-dispatcher.add_handler(echo_handler)
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://telegram-chatbot-v00.herokuapp.com' + TOKEN)
+    return "!", 200
 
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
