8fe5ff04fbd3c2d86a15dc9221f0377ec62925f5
jeter02 <sj.0713@gmail.com>
2019-10-31 01:07:51 +0900

    test 4

--- chatbot_v00.py
+++ chatbot_v00.py
@@ -9,14 +9,14 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
-##@bot.message_handler(commands=['start'])
-##def start(message):
-##    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-##@bot.message_handler(func=lambda message: True, content_types=['text'])
-##def echo_message(message):
-##    bot.reply_to(message, message.text)
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
@@ -30,9 +30,6 @@ def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://telegram-chatbot-v00.herokuapp.com' + TOKEN)
     return "!", 200
-@bot.message_handler(commands=['help']) # help message handler
-def send_welcome(message):
-    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
